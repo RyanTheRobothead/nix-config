3356e0c0503024f921412410ec9965e4d7194fbc
Ryan Lewis <accounts+github@ryandlewis.dev>
2026-06-03 21:45:08 -0500

    Configure printers

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -110,6 +110,12 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = [ pkgs.hplipWithPlugin ];
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
